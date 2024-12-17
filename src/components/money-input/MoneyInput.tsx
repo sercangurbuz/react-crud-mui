9d682a306589dffb57895062c04d9308aae8a9a9
sercan gurbuz <sercangurbuz@msn.com>
2024-12-17 15:55:19 +0300

    .

--- src/components/money-input/MoneyInput.tsx
+++ src/components/money-input/MoneyInput.tsx
@@ -1,6 +1,5 @@
 import { forwardRef } from 'react';
 
-import { DEFAULT_DECIMAL_DIGIT } from '../misc/currencyFormat';
 import { Currency, DEFAULT_CURRENCY, getCurrencySymbolProps } from '../misc/getCurrencySymbolProps';
 import NumberInput, { NumberInputProps } from '../number-input/NumberInput';
 
@@ -12,12 +11,7 @@ export type MoneyInputProps = NumberInputProps & { currency?: Currency };
 
 function MoneyInput({ getRef, currency = DEFAULT_CURRENCY, ...rest }: MoneyInputProps) {
   return (
-    <NumberInput
-      decimalScale={DEFAULT_DECIMAL_DIGIT}
-      {...getCurrencySymbolProps(currency)}
-      {...rest}
-      ref={getRef}
-    />
+    <NumberInput decimalScale={2} {...getCurrencySymbolProps(currency)} {...rest} ref={getRef} />
   );
 }
 
